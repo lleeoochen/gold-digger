a870ca0fb56e466598137226fa8841d895bac914
Wei Tung Chen <lleeoochen@gmail.com>
2018-01-31 12:53:02 -0800

    Added subject iteration and html page loading to output files

--- scraper.py
+++ scraper.py
@@ -8,8 +8,21 @@ catalog_url = 'https://my.sa.ucsb.edu/public/curriculum/coursesearch.aspx'
 browser = webdriver.Chrome(executable_path=chrome_driver)
 browser.get(catalog_url)
 
-subjects = Select(browser.find_element_by_name('ctl00$pageContent$courseList'))
-options = subjects.options
+# Collect all subject names
+subject_list = []
+subject_html = Select(browser.find_element_by_id('ctl00_pageContent_courseList'))
+for option in subject_html.options:
+	subject_list.append(option.get_attribute("value"))
 
-for option in options:
-	subjects.select_by_value(option.get_attribute("value"))
+# Click through subjects
+i = 0
+for subject in subject_list:
+	subject_html = Select(browser.find_element_by_id('ctl00_pageContent_courseList'))
+	subject_html.select_by_value(subject)
+	submit = browser.find_element_by_id('ctl00_pageContent_searchButton')
+	submit.click()
+
+	# Collect data
+	file = open("output/" + str(i) + ".html", "w")
+	file.write(browser.page_source.encode('utf-8'))
+	i = i + 1
