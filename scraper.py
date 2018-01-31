8672cc0d40caac0f41b2298d337de7bbbe0265ff
Wei Tung Chen <lleeoochen@gmail.com>
2018-01-31 13:45:07 -0800

    Refactor code and add quarter into combination

--- scraper.py
+++ scraper.py
@@ -2,25 +2,44 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
-chrome_driver = './chromedriver'
-catalog_url = 'https://my.sa.ucsb.edu/public/curriculum/coursesearch.aspx'
+# Setup constants
+CHROME_DRIVER = './chromedriver'
+CATALOG_URL = 'https://my.sa.ucsb.edu/public/curriculum/coursesearch.aspx'
+SUBJECT_ID = 'ctl00_pageContent_courseList'
+QUARTER_ID = 'ctl00_pageContent_quarterList'
+SEARCH_ID = 'ctl00_pageContent_searchButton'
 
-browser = webdriver.Chrome(executable_path=chrome_driver)
-browser.get(catalog_url)
 
 # Collect all subject names
-subject_list = []
-subject_html = Select(browser.find_element_by_id('ctl00_pageContent_courseList'))
-for option in subject_html.options:
-	subject_list.append(option.get_attribute("value"))
+def get_list(browser, element_id):
+	newlist = []
+	html = Select(browser.find_element_by_id(element_id))
+	for option in html.options:
+		newlist.append(option.get_attribute("value"))
+	return newlist
+
+
+# Setup selenium
+browser = webdriver.Chrome(executable_path=CHROME_DRIVER)
+browser.get(CATALOG_URL)
+
+# Get lists
+subject_list = get_list(browser, SUBJECT_ID)
+quarter_list = get_list(browser, QUARTER_ID)
+
 
 # Click through subjects
 for subject in subject_list:
-	subject_html = Select(browser.find_element_by_id('ctl00_pageContent_courseList'))
-	subject_html.select_by_value(subject)
-	submit = browser.find_element_by_id('ctl00_pageContent_searchButton')
-	submit.click()
+	for quarter in quarter_list:
 
-	# Collect data
-	file = open("output/" + subject + ".html", "w")
-	file.write(browser.find_element_by_tag_name('center').get_attribute('innerHTML').encode('utf-8'))
+		# Select subject, quarter
+		Select(browser.find_element_by_id(SUBJECT_ID)).select_by_value(subject)
+		Select(browser.find_element_by_id(QUARTER_ID)).select_by_value(quarter)
+
+		# Click on search button
+		submit = browser.find_element_by_id(SEARCH_ID)
+		submit.click()
+
+		# Collect data
+		file = open("output/" + quarter + "_" + subject + ".html", "w")
+		file.write(browser.find_element_by_tag_name('center').get_attribute('innerHTML').encode('utf-8'))
