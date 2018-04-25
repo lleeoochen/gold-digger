7efec32b0cb4e1b25685cc296e70f8d01af9740c
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-25 00:54:22 -0700

    Added a sample functional communication between server and client

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var path = require('path');
+var app = express();
 var PORT = process.env.PORT || 5000;
 
-express()
-	.use(express.static(path.join(__dirname, 'public')))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var count = 0;
 
+//Host static website from public/
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Get request for getting graph data
+app.get('/getGraph', function (req, res) {
+	res.send("Here is your graph from server! " + (count++) + " times!");
+});
+
+//Start server at PORT
+var server = app.listen(PORT, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log(`Listening on ${ port }.`);
+});
