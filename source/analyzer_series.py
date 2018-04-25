46230242286b0481bb9229a4be7db678c5699ad2
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-25 00:22:48 -0700

    Added cleaned data and graph data.

--- source/analyzer_series.py
+++ source/analyzer_series.py
@@ -7,6 +7,7 @@ import os
 data_dir = './DataCrontab/'
 cleaned_data = './Output/cleaned_data.csv'
 
+
 def getAllFiles(dir):
     files = []
     listing = os.listdir(dir)
@@ -19,24 +20,40 @@ files = getAllFiles(data_dir)
 files.sort()
 
 
+data_length = 0
 enrollments = {}
 for file in files:
+
     print ('Parsing ' + file)    
     os.system('python cleaner.py ' + file)
     df = pd.read_csv(cleaned_data)
 
     for index, row in df.iterrows():
         
-        if (int(row['Monday']) + int(row['Tuesday']) 
-            + int(row['Wednesday']) + int(row['Thursday']) 
-            + int(row['Friday']) > 1):
-                enrollment = row['RatioEnroll']
-                name = str(row['Subject']) + '-' + str(row['ID'])
-                
-                if not enrollments.has_key(name):
-                    enrollments[name] = []
+        if (int(row['Monday']) + int(row['Tuesday']) + int(row['Wednesday']) + int(row['Thursday']) + int(row['Friday']) > 1):
+            enrollment = round(row['RatioEnroll'], 5)
+            name = str(row['Subject']) + '-' + str(row['ID'])
+
+            if not enrollments.has_key(name):
+                enrollments[name] = []
+
+            if len(enrollments[name]) == data_length:
                 enrollments[name].append(enrollment)
-    
-plt.plot(np.arange(len(enrollments.get('WRIT-50'))), enrollments.get('WRIT-50'))
+
+    data_length += 1
+
+
+plt.plot(np.arange(len(enrollments.get('PHYS-2'))), enrollments.get('PHYS-2'))
 plt.ylabel('some numbers')
 plt.show()
+
+
+file = open("DataGraph/data.csv","w")
+for key in enrollments:
+    output = key + ",["
+    for value in enrollments[key]:
+        output += str(value) + ","
+    output = output[:-1] + "]\n"
+    file.write(output)
+
+file.close()
