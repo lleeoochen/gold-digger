ac5bac943a6282f14523b3cdc5c7588b04743cd6
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-24 23:09:07 -0700

    Removed unnecessary auto-generated files

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var path = require('path');
 var PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+	.use(express.static(path.join(__dirname, 'public')))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
