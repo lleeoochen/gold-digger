e792e10d125b37847b727b8d25686a664ab92ee4
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-24 22:50:48 -0700

    Moved scrips and data files to source

--- Resource/install_selenium.py
+++
@@ -1,4 +0,0 @@
-import os
-
-#Install selenium. See https://pypi.python.org/pypi/selenium.
-os.system('pip install -U selenium')
--- driver.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system('python scraper.py')
-os.system('python3 parser.py')
-os.system('rm Output/*.html')
