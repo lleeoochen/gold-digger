b9d739ab48bd7d3cbe31b7efdc4f5520b6c06fc3
Wei Tung Chen <lleeoochen@gmail.com>
2018-05-01 22:33:16 -0700

    Added data visualization.

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Get request for getting subjects
 app.get('/getSubjects', function (req, res) {
-	res.send(Object.keys(graphData));
+	res.send(Object.keys(graphData).sort());
 });
 
 //Get request for getting courses
 app.get('/getCourses', function (req, res) {
-	res.send(Object.keys(graphData[req.query.subject]));
+	res.send(Object.keys(graphData[req.query.subject]).sort(course_comparator));
 });
 
 //Get request for getting graph data
@@ -42,7 +42,7 @@ function loadGraphData() {
 		.on('line', function (line) {
 			try {
 				let commaIndex = line.indexOf(',');
-				let dashIndex = line.indexOf('-');
+				let dashIndex = line.lastIndexOf('-');
 				let subject = line.substring(0, dashIndex);
 				let courseID = line.substring(dashIndex + 1, commaIndex);
 				let enrollData = (line.includes('nan')) ? [] : JSON.parse(line.substring(commaIndex + 1));
@@ -59,4 +59,27 @@ function loadGraphData() {
 			console.log('Graph data loaded.');
 			// console.log(graphData);
 		});
+}
+
+//Sort comparator
+function course_comparator(a, b) {
+	let acode = "Z", bcode = "Z";
+
+	if (a.charCodeAt(a.length - 1) > "9".charCodeAt(0)) {
+		acode = a[a.length - 1];
+		a = a.substr(0, a.length - 1);
+	}
+
+	if (b.charCodeAt(b.length - 1) > "9".charCodeAt(0)) {
+		bcode = b[b.length - 1];
+		b = b.substr(0, b.length - 1);
+	}
+
+	if (a.length !== b.length)
+		return a.length - b.length;
+
+	if (a !== b)
+		return a - b;
+
+	return acode.charCodeAt(0) - bcode.charCodeAt(0);
 }
